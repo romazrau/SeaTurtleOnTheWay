2b5cb499b18276593475d2de67bf28947bf16257
romazrau <b04208017@ntu.edu.tw>
2020-08-08 23:44:20 +0800

    feat: C# Asp MVC + Web API 新建

---
+++ AspSeaTurtleOnTheWay/AspSeaTurtleOnTheWay/Controllers/ApiMemberController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AspSeaTurtleOnTheWay.Controllers
+{
+    public class ApiMemberController : ApiController
+    {
+        // GET: api/ApiMember
+        public List<tMember> Get()
+        {
+            SeaTurtleOnTheWayEntities db = new SeaTurtleOnTheWayEntities();
+            var s = from t in db.tMember
+                    select t;
+            List<tMember> outlist = s.ToList();
+            //因為有null所以噴錯(?
+
+
+            return outlist;
+        }
+
+
+
+
+        // GET: api/ApiMember/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ApiMember
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/ApiMember/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ApiMember/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ AspSeaTurtleOnTheWay/AspSeaTurtleOnTheWay/Controllers/MemberController.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspSeaTurtleOnTheWay.Controllers
+{
+    public class MemberController : Controller
+    {
+
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public string Member()
+        {
+            SeaTurtleOnTheWayEntities db = new SeaTurtleOnTheWayEntities();
+            var s = from t in db.tMember
+                    select t;
+            List<tMember> outlist = s.ToList();
+
+            try
+            {
+                string jsonData = JsonConvert.SerializeObject(s, Formatting.Indented,
+                               new JsonSerializerSettings
+                               {
+                                   PreserveReferencesHandling = PreserveReferencesHandling.Objects
+                               });
+
+
+                return jsonData;
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+
+        [HttpPost]
+        public string Transcripts(FormCollection post)
+        {
+            string account = post["account"];
+            string password = post["password"];
+           
+            return $"{account} , {password}";
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
