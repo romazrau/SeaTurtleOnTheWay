bbcad00f48748d0c196bd4e27b04b71f73928288
dada0508 <60784178+dada0508@users.noreply.github.com>
2020-08-31 10:32:57 +0800

    MVC範例
    MVC範例

---
+++ test1/test1/Viewmodel/Memberlist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using test1.Models;
+
+namespace test1.Viewmodel
+{
+    public class Memberlist
+    {
+        public int fId { get; set; }
+        public string fAccount { get; set; }
+        public string fName { get; set; }
+
+        public string fAccountType { get; set; }
+    }
+}
