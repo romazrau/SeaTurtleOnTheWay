d26f22f9e67197c75bcb3f675ac0fd503c49af62
dada0508 <60784178+dada0508@users.noreply.github.com>
2020-09-03 10:48:23 +0800

    Revert "刪除"
    This reverts commit ac220e4a9d50600eb59acb2510064a46cd74ac11.

---
+++ backe/backe/Models/MemberListDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace backe.Models
+{
+    public class MemberListDetail
+    {
+        public int fId { get; set; }
+        public string fName { get; set; }
+        public string fAccount { get; set; }
+        public int fCoins { get; set; }
+
+        
+        public string fAccountType { get; set; }
+        
+
+    }
+}
