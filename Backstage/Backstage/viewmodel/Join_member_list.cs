1ca7df2ad0eb25da30e1c5e8a715d829871d642e
dada0508 <60784178+dada0508@users.noreply.github.com>
2020-09-03 19:32:07 +0800

    MVC活動詳細資料
    MVC活動詳細資料

---
+++ Backstage/Backstage/viewmodel/Join_member_list.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backstage.viewmodel
+{
+    public class Join_member_list
+    {
+        public int fId { get; set; }
+
+        public string fActivityName { get; set; }
+
+        public string fMemberName { get; set; }
+
+        public string fJoinTime { get; set; }
+
+        public string fJoinTypeName { get; set; }
+    }
+}
