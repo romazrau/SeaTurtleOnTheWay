425912de753075c8051b654d2a8b699f6cd2d2dd
a2014946 <a2014946@gmail.com>
2020-09-17 23:43:59 +0800

    名稱

--- Backstage/Backstage/viewmodel/Join_member_list.cs
+++ Backstage/Backstage/viewmodel/Join_member_list.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,14 +8,16 @@ namespace Backstage.viewmodel
 {
     public class Join_member_list
     {
+        [DisplayName("序")]
         public int fId { get; set; }
-
+        [DisplayName("活動名稱")]
         public string fActivityName { get; set; }
+        [DisplayName("會員名稱")]
 
         public string fMemberName { get; set; }
-
+        [DisplayName("加入時間")]
         public string fJoinTime { get; set; }
-
+        [DisplayName("類別")]
         public string fJoinTypeName { get; set; }
     }
 }
